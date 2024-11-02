f72e7fa40c4fc4a461d9f9b9f1ed4c0ce3d33152
1024ent <shonenent1024@gmail.com>
2024-11-03 00:59:43 +0800

    Updated PotholeDetection.py

--- PotholeDetection/PotholeDetection.py
+++ PotholeDetection/PotholeDetection.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 import numpy as np
-import pandas as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import random
@@ -102,4 +102,232 @@ plt.show()
 
 # Deleting unnecessary variable to free up memory
 del image_files
+
+# Train the model on our custom dataset
+results = model.train(
+    data = yaml_file_path,      # Path to the dataset configuretion file
+    epochs = 150,               # Number of training epochs
+    imgsz = 640,                # Size of input images as integer
+    patience = 15,              # Epochs to wait for no observable improvement for early stopping of training
+    batch = 16,                 # Batch size
+    optimizer = 'auto',         # Optimizer to use, choices=[SGD, Adam, Adamax, AdamW, NAdam, RAdam, RMSProp, auto]
+    lr0 = 0.0001,               # Initial learning rate
+    lrf = 0.01,                 # Final learning rate (lr0 * lrf)
+    dropout = 0.25,             # Use dropout regularization
+    device = 0,                 # Device to run on, i.e. cuda device=0
+    seed=42                     # Random seed for reproducibility
+)
+
+# Define the path to the directory
+post_training_files_path = os.path.join(dataset_path, 'train', 'outputs')
+
+# List the file in the directory
+try:
+    files = os.listdir(post_training_files_path)
+    print("Files in the post-training directory:")
+    for file in files:
+        print(file)
+except FileNotFoundError:
+    print(f"The directory {post_training_files_path} does not exist.")
+except Exception as e:
+    print(f"An error occured: {e}")
+
+# Create the full file path by joining the directory path with the filename
+results_file_path = os.path.join(post_training_files_path, 'results.png')
+
+# Read the image using cv2
+image = cv2.imread(results_file_path)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Display the image using matplotlib
+plt.figure(figsize=(20, 8))
+plt.imshow(image)
+plt.title('Training and Validation Loss Trends', fontsize=24)
+plt.axis('off')
+plt.show()
+
+# Define a function to plot learning curves for loss values
+def plot_learning_curve(df, train_loss_col, val_loss_col, title, ylim_range=[0,2]):
+    plt.figure(figsize=(12, 4))
+    sns.lineplot(data=df, x='epoch', y=train_loss_col, label='Train Loss', color='blue', linestyle='-', linewidth=2)
+    sns.lineplot(data=df, x='epoch', y=val_loss_col, label='Validation Loss', color='#ed2f00', linestyle='--', linewidth=2)
+    plt.title(title)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.ylim(ylim_range)
+    plt.legend()
+    plt.show()
+
+# Create the full file path for 'result.csv' using the directory path and file name
+results_csv_path = os.path.join(post_training_files_path, 'results.csv')
+
+# Load the CSV file from the constructed path into a pandas DataFrame
+df = pd.read_csv(results_csv_path)
+
+# Remove any leading whitespace from the column names
+df.columns = df.columns.str.strip()
+
+# Plot the learning curves for each loss
+plot_learning_curve(df, 'train/box_loss', 'val/box_loss', 'Bounding Box Loss Learning Curve')
+plot_learning_curve(df, 'train/cls_loss', 'val/cls_loss', 'Classification Loss Learning Curve')
+plot_learning_curve(df, 'train/dfl_loss', 'val/dfl_loss', 'Distribution Focal Loss Learning Curve')
+plot_learning_curve(df, 'train/seg_loss', 'val/seg_loss', 'Segmentation Loss Learning Curve', ylim_range=[0,5])
+
+# Define the filenames for 'Box' and 'Mask' metrics along with their titles
+box_files_titles = {
+    'BoxP_curve.png': 'Bounding Box Precision-Confidence Curve',
+    'BoxR_curve.png': 'Bounding Box Recall-Confidence Curve',
+    'BoxF1_curve.png': 'Bounding Box F1-Confidence Curve'
+}
+mask_files_titles = {
+    'MaskP_curve.png': 'Mask Precision-Confidence Curve',
+    'MaskR_curve.png': 'Mask Recall-Confidence Curve',
+    'MaskF1_curve.png': 'Mask F1-Confidence Curve'
+}
+
+# Create a 3x2 subplot
+fig, axs = plt.subplots(3, 2, figsize=(20, 20))
+
+# Function to read and convert image for plotting
+def read_and_convert_image(file_path):
+    # Read the image using cv2
+    image = cv2.imread(file_path)
+    # Convert from BGR to RGB
+    return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+# Plot 'Box' images in the first column with meaningful titles
+for i, (filename, title) in enumerate(box_files_titles.items()):
+    img_path = os.path.join(post_training_files_path, filename)
+    img = read_and_convert_image(img_path)
+    axs[i, 0].imshow(img)
+    axs[i, 0].set_title(title, fontsize=20)
+    axs[i, 0].axis('off') 
+
+# Plot 'Mask' images in the second column with meaningful titles
+for i, (filename, title) in enumerate(mask_files_titles.items()):
+    img_path = os.path.join(post_training_files_path, filename)
+    img = read_and_convert_image(img_path)
+    axs[i, 1].imshow(img)
+    axs[i, 1].set_title(title, fontsize=20)
+    axs[i, 1].axis('off')  
+
+plt.tight_layout()
+plt.show()
+
+# Define the filenames for 'Box' and 'Mask' metrics along with their titles
+pr_files_titles = {
+    'BoxPR_curve.png': 'Bounding Box Precision-Recall Curve',
+    'MaskPR_curve.png': 'Mask Precision-Recall Curve'
+}
+
+# Create a 1x2 subplot
+fig, axs = plt.subplots(1, 2, figsize=(20, 10))
+
+# Plot 'Box' and 'Mask' images in the subplot with meaningful titles
+for i, (filename, title) in enumerate(pr_files_titles.items()):
+    img_path = os.path.join(post_training_files_path, filename)
+    img = read_and_convert_image(img_path)
+    axs[i].imshow(img)
+    axs[i].set_title(title, fontsize=20)
+    axs[i].axis('off')
+
+plt.tight_layout()
+plt.show()
+
+# Construct the path to the confusion matrix images
+confusion_matrix_path = os.path.join(post_training_files_path, 'confusion_matrix.png')
+confusion_matrix_normalized_path = os.path.join(post_training_files_path, 'confusion_matrix_normalized.png')
+
+# Create a 1x2 subplot
+fig, axs = plt.subplots(1, 2, figsize=(20, 10))
+
+# Read and convert both images
+cm_img = read_and_convert_image(confusion_matrix_path)
+cm_norm_img = read_and_convert_image(confusion_matrix_normalized_path)
+
+# Display the images
+axs[0].imshow(cm_img)
+axs[0].set_title('Confusion Matrix', fontsize=24)
+axs[0].axis('off') 
+
+axs[1].imshow(cm_norm_img)
+axs[1].set_title('Normalized Confusion Matrix', fontsize=24)
+axs[1].axis('off')
+
+plt.tight_layout()
+plt.show()
+
+
+
+# Construct the path to the best model weights file using os.path.join
+best_model_path = os.path.join(post_training_files_path, 'weights/best.pt')
+
+# Load the best model weights into the YOLO model
+best_model = YOLO(best_model_path)
+
+# Validate the best model using the validation set with default parameters
+metrics = best_model.val(split='val')
+
+# Convert the dictionary to a pandas DataFrame and use the keys as the index
+metrics_df = pd.DataFrame.from_dict(metrics.results_dict, orient='index', columns=['Metric Value'])
+
+# Display the DataFrame
+metrics_df.round(3)
+
+# Define the path to the validation images
+valid_images_path = os.path.join(dataset_path, 'valid', 'images')
+
+# List all jpg images in the directory
+image_files = [file for file in os.listdir(valid_images_path) if file.endswith('.jpg')]
+
+# Select 9 images at equal intervals
+num_images = len(image_files)
+selected_images = [image_files[i] for i in range(0, num_images, num_images // 9)]
+
+# Initialize the subplot
+fig, axes = plt.subplots(3, 3, figsize=(20, 21))
+fig.suptitle('Validation Set Inferences', fontsize=24)
+
+# Perform inference on each selected image and display it
+for i, ax in enumerate(axes.flatten()):
+    image_path = os.path.join(valid_images_path, selected_images[i])
+    results = best_model.predict(source=image_path, imgsz=640)
+    annotated_image = results[0].plot()
+    annotated_image_rgb = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
+    ax.imshow(annotated_image_rgb)
+    ax.axis('off')
+
+plt.tight_layout()
+plt.show()
+
+# Define the path to the sample video in the dataset
+dataset_video_path = os.path.join('Pothole_Segmentation_YOLOv8', 'sample_video.mp4')
+
+# Define the destination path in the working directory
+video_path = 'Pothole_Segmentation_YOLOv8\output_video.mp4'
+
+# Copy the video file from its original location in the dataset to the current working directory
+shutil.copyfile(dataset_video_path, video_path)
+
+# Initiate vehicle detection on the sample video using the best performing model and save the output
+best_model.predict(source=video_path, save=True)
+
+# Convert the .avi video generated by the YOLOv8 prediction to .mp4 format for compatibility
+output_video_path = 'processed_sample_video.mp4'
+os.system(f'ffmpeg -y -loglevel panic -i "runs/segment/predict/sample_video.avi" "{output_video_path}"')
+
+# Display the processed sample video using OpenCV or an appropriate method in your local environment
+# Note: Replace this part with your preferred method of displaying video, as this code may not be directly executable in all environments.
+cap = cv2.VideoCapture(output_video_path)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+    cv2.imshow('Processed Sample Video', frame)
+    if cv2.waitKey(25) & 0xFF == ord('q'):  # Press 'q' to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
